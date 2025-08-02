e33a9ea221fb34c71de3a0144cfcc274886d2a82
NasDev07 <nasruddin20182019@gmail.com>
2025-08-02 22:43:37 +0700

    project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import KiwiSmartApp from './components/KiwiSmartApp'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <KiwiSmartApp />
+    </div>
+  )
+}
+
+export default App
